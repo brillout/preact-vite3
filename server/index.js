5ee46afacab2409ead6e6b0b9a2da2cad3e25058
Romuald Brillout <git@brillout.com>
2022-06-08 08:58:07 +0200

    update example

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const compression = require('compression')
-const { createPageRenderer } = require('vite-plugin-ssr')
+const { renderPage } = require('vite-plugin-ssr')
 
 const isProduction = process.env.NODE_ENV === 'production'
 const root = `${__dirname}/..`
@@ -12,23 +12,21 @@ async function startServer() {
 
   app.use(compression())
 
-  let viteDevServer
   if (isProduction) {
     app.use(express.static(`${root}/dist/client`))
   } else {
     const vite = require('vite')
-    viteDevServer = await vite.createServer({
+    const viteDevServer = await vite.createServer({
       root,
-      server: { middlewareMode: 'ssr' },
+      server: { middlewareMode: 'ssr' }
     })
     app.use(viteDevServer.middlewares)
   }
 
-  const renderPage = createPageRenderer({ viteDevServer, isProduction, root })
   app.get('*', async (req, res, next) => {
     const url = req.originalUrl
     const pageContextInit = {
-      url,
+      url
     }
     const pageContext = await renderPage(pageContextInit)
     const { httpResponse } = pageContext
